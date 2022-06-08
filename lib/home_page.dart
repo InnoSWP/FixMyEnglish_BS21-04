5c883abc77e005826b0072a7a044b7143abde51d
Smulemun <pokemon88005553535@gmail.com>
2022-06-08 23:07:08 +0300

    fixed file upload

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,19 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 import 'file.dart';
 import 'mistake_page.dart';
 import 'mistake_api.dart';
+
+Future<FilePickerResult?> mistakeFromPDF() async {
+  FilePickerResult? result = await FilePicker.platform.pickFiles(
+    allowMultiple: true,
+    type: FileType.custom,
+    allowedExtensions: ['pdf'],
+  );
+  return result;
+}
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -33,8 +44,23 @@ class HomePage extends StatelessWidget {
           Row(
             children: [
               ElevatedButton(
-                onPressed: () {
-                  // to be done
+                onPressed: () async {
+                  List<Future<MistakeFile>> files = [];
+                  FilePickerResult? result =
+                      await FilePicker.platform.pickFiles(
+                    allowMultiple: true,
+                    type: FileType.custom,
+                    allowedExtensions: ['pdf'],
+                  );
+                  if (result != null) {
+                    for (final file in result.files) {
+                      final PdfDocument document =
+                          PdfDocument(inputBytes: file.bytes);
+                      String text = PdfTextExtractor(document).extractText();
+                      files.add(mistakeFromAPI(text, file.name));
+                    }
+                    redirectToMistakePage(context, files);
+                  }
                 },
                 child: const Text('Upload Files'),
               ),
